77d2b0c24c038cf4662a0510d2a51b1b0d5e89ef
Takuya Tominaga <takuya1.tominaga@leach.co.jp>
2025-07-10 13:55:49 +0900

    add auth

--- pdf-front-app/src/app/providers.tsx
+++ pdf-front-app/src/app/providers.tsx
@@ -1,7 +1,32 @@
 "use client";
 
 import { HeroUIProvider } from "@heroui/react";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ReactNode } from "react";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { useAuth } from "@clerk/nextjs";
+
+if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
+  throw new Error("Missing NEXT_PUBLIC_CONVEX_URL in your .env file");
+}
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
+
+function ConvexClientProvider({ children }: { children: ReactNode }) {
+  return (
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+      {children}
+    </ConvexProviderWithClerk>
+  );
+}
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <HeroUIProvider>{children}</HeroUIProvider>;
+  return (
+    <ClerkProvider>
+      <HeroUIProvider>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
+      </HeroUIProvider>
+    </ClerkProvider>
+  );
 }
